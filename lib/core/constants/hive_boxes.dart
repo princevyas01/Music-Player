9d6c07b0b063e3fae6a0462fc89440255cb4aed7
Prince Vyas <princevyas13456@gmail.com>
2026-09-10 22:58:57 +0530

    chore(config): update Hive Boxes

--- lib/core/constants/hive_boxes.dart
+++ lib/core/constants/hive_boxes.dart
@@ -6,4 +6,16 @@ class HiveBoxes {
   static const String history = 'historyBox';
   static const String metadataOverrides = 'metadataOverridesBox';
   static const String queueState = 'queueStateBox';
+
+  // New domain boxes
+  static const String artworkCache = 'artworkCacheBox';
+  static const String waveformCache = 'waveformCacheBox';
+  static const String lyrics = 'lyricsBox';
+  static const String sessions = 'sessionsBox';
+  static const String recommendationCache = 'recommendationCacheBox';
+  static const String smartRules = 'smartRulesBox';
+  static const String audioEffects = 'audioEffectsBox';
+  static const String scheduledPlayback = 'scheduledPlaybackBox';
+  static const String sync = 'syncBox';
+  static const String playlistFolders = 'playlistFoldersBox';
 }
